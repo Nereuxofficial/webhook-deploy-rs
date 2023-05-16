9bbb2ff0547e310675430dc1bc52689ec889134b
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-05-16 15:45:04 +0200

    Added actix-web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use actix_web::{App, HttpServer};
+#[get("/")]
+async fn index() -> &'static str {
+    "Hello world!"
+}
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()>{
@@ -5,6 +10,12 @@ async fn main() -> color_eyre::Result<()>{
     color_eyre::install()?;
     dotenvy::dotenv().ok();
     // Webserver using actix-web
-
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await;
     Ok(())
 }
