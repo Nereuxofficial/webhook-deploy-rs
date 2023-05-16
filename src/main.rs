36464477fbd92a1d52fbadc0747cc3f3cec52f57
Bene <37740907+Nereuxofficial@users.noreply.github.com>
2023-05-16 22:39:54 +0200

    Added service i missed because stupid

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod command;
 mod config;
 
 use crate::config::load_configs;
+use actix_web::http::header::HeaderValue;
 use actix_web::http::Uri;
 use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
@@ -9,6 +10,10 @@ use serde_json::Value;
 use std::collections::HashMap;
 use std::env;
 use tracing::info;
+
+// TODO: If we process the first payload we can inform about new webhooks connected to the server
+// TODO: Admin Panel to add new webhooks, remove old ones and restart them
+// TODO: Admin Panel to display logs, and show the status of the scripts
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 struct Application {
@@ -21,7 +26,6 @@ struct Application {
 async fn index() -> &'static str {
     "Hello world!"
 }
-
 /// Accept and validate the payload from github
 #[post("/payload")]
 async fn payload(
@@ -29,13 +33,21 @@ async fn payload(
     bytes: web::Bytes,
     data: web::Data<HashMap<Uri, Application>>,
 ) -> HttpResponse {
-    // TODO: Fix unwraps
+    if req
+        .headers()
+        .get("X-GitHub-Event")
+        .unwrap_or(&HeaderValue::from_str("").unwrap())
+        != HeaderValue::from_str("push").unwrap()
+    {
+        return HttpResponse::Ok().body("Not a push event!");
+    }
+    // TODO: Handle unwraps
     // Get repository URL
     let body = String::from_utf8(bytes.to_vec()).unwrap();
     info!("Payload: {}", body);
     let json: Value = serde_json::from_str(&body).unwrap();
-    let repo_url = json.get("repository");
-    info!("Repo URL: {}", repo_url.unwrap());
+    let repo_url: String = json.get("html_url").unwrap().to_string();
+    info!("Repo URL: {}", repo_url);
     let signature = req
         .headers()
         .get("X-Hub-Signature-256")
@@ -52,10 +64,12 @@ async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     dotenvy::dotenv().ok();
     // Webserver using actix-web
+    info!("Starting webserver");
     HttpServer::new(|| {
         App::new()
             .app_data(web::Data::new(|| async move { load_configs().await }))
             .service(index)
+            .service(payload)
     })
     .bind(env::var("BIND_IP").unwrap())?
     .run()
